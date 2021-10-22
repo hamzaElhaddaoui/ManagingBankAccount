6cdd9d59b365a5c3a79267047752200a93519c75
hamzaElhaddaoui <hamza.el.haddaoui@gmail.com>
2021-10-22 14:19:28 +0200

    init project

---
+++ src/main/java/com/bank/managingbankaccount/ManagingBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bank.managingbankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingBankAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingBankAccountApplication.class, args);
+    }
+
+}
